3683f89cb20f724e624246e25ade355b43b3cb19
Julia Korneva <amalia9111@gmail.com>
2020-12-22 22:21:22 +0200

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 50000;
-        int miles = service.calculate(price);
-        System.out.println(miles);
+        int Price = 50000;
+        int Miles = service.Calculate(Price);
+        System.out.println(Miles);
     }
 
 }
